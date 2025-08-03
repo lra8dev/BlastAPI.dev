23c0e5c36117deb0cfe11946cb676dd964316157
Laxman Rathod <laxmanrathod.dev@gmail.com>
2025-08-03 19:13:15 +0530

    perf: add fallback data

--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -1,10 +1,10 @@
 import { redirect } from "next/navigation";
 import { auth } from "@/auth";
 import { Header } from "@/components/header";
+import { fetchApi } from "@/lib/api";
 import { DashboardHeader } from "./_components/header";
 import { HeroCard } from "./_components/hero-card";
 import { TestsHistory } from "./_components/tests-history";
-import { fetchApi } from "@/lib/api";
 import { TestHistories } from "./_types";
 
 const Dashboard = async () => {
@@ -18,14 +18,14 @@ const Dashboard = async () => {
     cache: "no-store",
   });
 
-  const data = result.data ?? [];
+  const data = result?.data ?? [];
 
   return (
     <main className="flex flex-col w-full min-h-screen font-inter bg-white dark:bg-dark pb-14">
       <Header user={session.user} className="border-none" />
       <DashboardHeader data={data} />
       <TestsHistory data={data} />
-      <HeroCard isHistoryAvailable={data.length > 0} />
+      <HeroCard isHistoryAvailable={data.length !== 0} />
     </main>
   );
 };
