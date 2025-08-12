168094d1c59f2176d20cd92580f15b748405ddd0
Laxman Rathod <laxmanrathod.dev@gmail.com>
2025-08-13 01:39:12 +0530

    fix: add dotenv/config to package.json for initial environment variable loading

--- apps/server/src/config/redis.ts
+++ apps/server/src/config/redis.ts
@@ -2,16 +2,10 @@ import Redis from "ioredis";
 
 export const redis = new Redis({
   host: process.env.REDIS_HOST!,
-  port: parseInt(process.env.REDIS_PORT!, 10),
+  port: Number(process.env.REDIS_PORT!) || 6379,
   password: process.env.REDIS_PASSWORD!,
   maxRetriesPerRequest: null,
 });
-
-// FIXME: Env variables are not loading here...
-console.log("redis SERER PORT", process.env.SERVER_PORT);
-console.log("src/config loading Redis Host:", process.env.REDIS_HOST!);
-console.log("src/config loading Redis Port:", process.env.REDIS_PORT!);
-console.log("src/config loading Redis Password:", process.env.REDIS_PASSWORD!);
 
 redis.on("connect", () => {
   console.log("Connected to Redis Cloud! ✅");
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,16 +1,13 @@
-import dotenv from "dotenv";
 import http from "http";
 import app from "./app";
 import { initSocket } from "./lib/socket";
 
-dotenv.config();
-
 const server = http.createServer(app);
 const PORT = process.env.SERVER_PORT || 6000;
 
-// Initialize Socket.IO
 initSocket(server);
 
+// WIP: Handle errors, graceful shutdown and restart. Also listen on custom domain
 server.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
