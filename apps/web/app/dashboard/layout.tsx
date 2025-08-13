7bcdaa6bd5e81faa00a3e2fe05e2acb170b66f03
Laxman Rathod <laxmanrathod.dev@gmail.com>
2025-08-13 21:55:04 +0530

    feat: add layout with auth validation and header structure

---
+++ apps/web/app/dashboard/layout.tsx
@@ -0,0 +1,21 @@
+import { redirect } from "next/navigation";
+import { auth } from "@/auth";
+import { Header } from "@/components/header";
+import { LayoutProps } from "@/types/general";
+
+const DashboardLayout = async ({ children }: LayoutProps) => {
+  const session = await auth();
+
+  if (!session?.user) {
+    return redirect(`/signin?callbackUrl=${encodeURIComponent("/dashboard")}`);
+  }
+
+  return (
+    <main className="size-full font-inter bg-white dark:bg-dark">
+      <Header user={session.user} className="border-none" />
+      {children}
+    </main>
+  );
+};
+
+export default DashboardLayout;
