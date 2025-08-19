99a78073ab4c40ef5f1020f88cda5f6a5ee95336
Laxman Rathod <laxmanrathod.dev@gmail.com>
2025-08-19 17:07:22 +0530

    refactor: simplify type definations for dashboard

--- apps/web/app/dashboard/_types/index.ts
+++ apps/web/app/dashboard/_types/index.ts
@@ -1,72 +1,64 @@
 import { HealthCheckStatus, TestStatus, UserRole } from "@blastapi/db";
 import { TestRegions } from "@blastapi/validators";
-import { LucideIcon } from "lucide-react";
 
-export interface TestHistories {
-  data?: {
-    id: string;
-    status: TestStatus;
-    createdAt: Date;
-    user: {
-      name?: string;
-      email: string;
-      image?: string;
-      role: UserRole;
-    };
-    healthCheckSummary: {
-      passedChecks: number;
-      totalChecks: number;
-      overallStatus: HealthCheckStatus;
-    };
-    testConfig: { name: string; region: TestRegions; duration: number };
-    notes?: string[];
-  }[];
+interface User {
+  name: string | null;
+  email: string;
+  image: string | null;
+  role: UserRole;
 }
 
-export interface FilterBarProps {
-  data?: {
-    testConfig: { name: string };
-    user: { name?: string; email: string; image?: string; role: UserRole };
-  }[];
+export interface TestHistory {
+  id: string;
+  status: TestStatus;
+  createdAt: Date;
+  user: User;
+  testConfig: {
+    name: string;
+    region: TestRegions; // WIP: test region in DB should be enum (currently string)
+  } | null;
+  testResult: {
+    duration: number;
+    notes?: string[]; // WIP: Add notes
+  } | null;
+  healthCheckSummary: {
+    totalChecks: number;
+    passedChecks: number;
+    failedChecks: number;
+    overallStatus: HealthCheckStatus;
+  } | null;
+}
+
+export interface FilteringData {
+  test: { name?: string };
+  user: User;
 }
 
 interface FilterOption {
   id?: string;
   name?: string;
-  icon?: LucideIcon;
+  icon?: string;
   iconCN?: string;
   children?: React.ReactNode;
+  user?: {
+    url?: string;
+    email: string;
+  };
 }
 
 export interface PrimaryFilterConfig {
   key: string;
   label: string;
-  icon: LucideIcon;
+  icon: string;
   isCommandPallet?: boolean;
   options?: FilterOption[];
 }
-
-export type FilterProps =
-  | Record<
-      string,
-      {
-        testConfig: { name: string };
-        user: { name?: string; email: string; image?: string };
-      }
-    >
-  | undefined;
 
 export interface NavigationHandlers {
   hasSearchParam: (name: string, value?: string) => boolean;
   updateSearchParam: (name: string, value?: string | boolean) => void;
   getSearchParam?: (name: string) => string | null;
 }
-
-export interface PrimaryFiltersProps extends NavigationHandlers {
-  options: PrimaryFilterConfig[];
-}
-
-export interface ClearFilterProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 export interface TestBadge {
   notesLength?: number;
