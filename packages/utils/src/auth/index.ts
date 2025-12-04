6d13acb5f64bbb04638f2b5d798db7fc08f9999d
Laxman Rathod <laxmanrathod.dev@gmail.com>
2025-12-04 20:32:22 +0530

    feat: implement authentication utility methods
    - hashPassword() - for hashing plain password
    - validatePassword() - for comparing plain and hashed password

--- packages/utils/src/auth/index.ts
+++ packages/utils/src/auth/index.ts
@@ -1,10 +1,4 @@
-import { User } from "@blastapi/db";
 import { compare, hash } from "bcryptjs";
 
 export const hashPassword = (password: string, salt: number) => hash(password, salt);
 export const validatePassword = (plain: string, hashed: string) => compare(plain, hashed);
-export const safeUser = (user: User): User => {
-  const safeUserData = { ...user };
-  safeUserData.password = null;
-  return safeUserData;
-};
