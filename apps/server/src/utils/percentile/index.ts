0518c4f803fdf3ded4426a689d157216293b94f2
Laxman Rathod <laxmanrathod.dev@gmail.com>
2025-12-10 01:38:03 +0530

    chore: cleanup dependencies and add new ones

--- apps/server/src/utils/percentile/index.ts
+++
@@ -1,9 +0,0 @@
-export const percentile = (sortedArray: number[], p: number): number => {
-  if (sortedArray.length === 0) {
-    return 0;
-  }
-
-  const index = Math.ceil((p / 100) * sortedArray.length) - 1;
-
-  return sortedArray[Math.max(0, Math.min(index, sortedArray.length - 1))] || 0;
-};
