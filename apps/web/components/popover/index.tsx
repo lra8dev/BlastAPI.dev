b755128284fc4c26040972565fc50e6e20e3f270
Laxman Rathod <laxmanrathod.dev@gmail.com>
2025-12-10 12:55:00 +0530

    style: add asChild prop and improve style of CustPopover component

--- apps/web/components/popover/index.tsx
+++ apps/web/components/popover/index.tsx
@@ -19,12 +19,12 @@ export const CustPopover = ({
       </PopoverTrigger>
       <PopoverContent
         className={cn(
-          "w-fit max-w-[18rem] dark:bg-dark-5 border dark:border-neutral-700/45 px-1 py-1 rounded",
+          "w-fit max-w-[18rem] dark:bg-dark-5 border dark:border-neutral-700/40 px-1 py-1 rounded",
           contentClassName,
         )}
         align={align}
       >
-        <PopoverClose>
+        <PopoverClose asChild>
           <Card
             className={cn(
               "font-inter font-medium text-[.8125rem] gap-0.5 text-neutral-600 dark:text-gray-300/80  p-0 m-0 rounded-none border-none bg-transparent dark:bg-transparent shadow-none",
